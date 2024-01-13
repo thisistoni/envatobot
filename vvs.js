213571f6aac979905c13f8ce08b84cbb90174ed5
Antonio Beslic <thisistoni30@gmail.com>
2024-01-13 23:22:03 +0100

    Bug fix - updated incoming messages into VVS account

--- vvs.js
+++ vvs.js
@@ -23,18 +23,25 @@ async function getDownloadLinkFromEnvato(envatoLink) {
         await client.sendMessage('@eedownloader_bot', { message: envatoLink });
 
         // Warte auf die Antwort vom Envato Bot
+        const startTime = Date.now();   
+        
         let downloadLinkMessage = null;
         let downloadCountMessage = null;
         while (!downloadLinkMessage || !downloadCountMessage) {
+            if (Date.now() - startTime > 30000) {
+                console.log('Zeitlimit erreicht, Abbruch der Schleife');
+                return ["Es gab einen Fehler beim Abrufen des Download-Links. Bitte versuche es später noch einmal."];
+
+            }
             let messages = await client.getMessages('@eedownloader_bot', { limit: 2 });
             let message = messages[0].message;
-            if (message.startsWith('https://l.envato-download.com/')) {
+            if (message.startsWith('https://yourl.cc/')) {
                 downloadLinkMessage = message;
                 if (downloadCountMessage == null && messages[1].message.includes('You’ve downloaded')) {
                     downloadCountMessage = messages[1].message;
 
                 }
-            } else if (message.includes('You’ve downloaded')) {
+            } else if (message.includes('Your downlaod link is')) {
                 downloadCountMessage = message;
             }
 
@@ -45,6 +52,7 @@ async function getDownloadLinkFromEnvato(envatoLink) {
 
         // Nach dem Aufrufen von client.disconnect()
         client._destroyed = true;
+
 
         let answers = [downloadLinkMessage];
         // Extrahiere die Anzahl der Downloads aus der Nachricht
